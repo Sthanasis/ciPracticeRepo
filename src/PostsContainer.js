4cea230dcf73d2adcdf54f0c35719170a82a2978
Sthanasis <sthanasis1994@gmail.com>
2022-01-27 15:55:04 +0200

    Change widget cmp to posts compoenent

---
+++ src/PostsContainer.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View} from 'react-native';
+import api from './api';
+
+const PostsContainer = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    api.get('/posts').then((response) => {
+        setPosts(response.data);
+    }).catch(err => console.log(err));
+  }, []);
+
+  return (
+    <View>
+      {posts.map((post) => (
+        <Text key={post.id}>{post.title}</Text>
+      ))}
+    </View>
+  );
+};
+
+export default PostsContainer;
