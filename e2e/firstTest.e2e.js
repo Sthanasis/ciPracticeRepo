109cf7e2e18396294574e159c31199bb1fd24c14
Sthanasis <sthanasis1994@gmail.com>
2022-02-01 16:31:06 +0200

    android emulator e2e fix

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,9 +1,9 @@
-describe('First test',()=> {
-    beforeEach(async () => {
-        
-        await device.launchApp( );
-      });
-    it('should find the send button and tap it', async ()=> {
-        await element(by.id('sendButton')).tap();
-    })
-})
+describe('First test', () => {
+  beforeEach(async () => {
+    await device.launchApp();
+    await device.reloadReactNative();
+  });
+  it('should find the send button and tap it', async () => {
+    await element(by.id('sendButton')).tap();
+  });
+});
